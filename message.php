ec4d786638fcddb70b31d562c34237b997010b78
wsm1025 <64190019+wsm1025@users.noreply.github.com>
2021-03-08 19:41:55 +0800

    Add files via upload

--- message.php
+++ message.php
@@ -3,7 +3,12 @@ $dsn = "mysql:host=localhost;dbname=study;charset=utf8";
 $user = 'root';
 $pass = '980703';
 $pdo = new PDO($dsn,$user,$pass);
-$pdom = $pdo->query("select *  from w_room  ORDER BY id desc limit 1");//成功得到一个PDOStatement类 失败boolean(false)
+$pdos = $pdo->query("select id from w_room order by id desc limit 1");//成功得到一个PDOStatement类 失败boolean(false)
+$rex = $pdos->fetch(PDO::FETCH_ASSOC);
+$lastId = $rex["id"]?$rex["id"]:0;
+$length = $lastId - $_GET['id'];
+
+$pdom = $pdo->query("select *  from w_room  ORDER BY id asc limit {$_GET['id']},$length");//成功得到一个PDOStatement类 失败boolean(false)
 $res = $pdom->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($res);
 ?>
--- sendmessage.php
+++ sendmessage.php
@@ -3,7 +3,7 @@ $dsn = "mysql:host=localhost;dbname=study;charset=utf8";
 $user = 'root';
 $pass = '980703';
 $pdo = new PDO($dsn,$user,$pass);
-$row = $pdo->exec("insert w_room(message,nickname,createTime)values('{$_POST['content']}','{$_POST['name']}',now())");//成功得到一个PDOStatement类 失败boolean(false)
+$row = $pdo->exec("insert w_room(message,nickname,createTime,ip)values('{$_POST['content']}','{$_POST['name']}',now(),'{$_SERVER['REMOTE_ADDR']}')");//成功得到一个PDOStatement类 失败boolean(false)
 $id = $pdo->lastInsertId();
 if($id>0){
     $data = array ('msg'=>"ok",'status'=>1,'id'=>$id);
